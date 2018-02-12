9c5df130d986705967e8741b636dd96ef062b8fa
UP773455 <up773455@myport.ac.uk>
2018-02-12 16:44:34 +0000

    modified:   index.html 	modified:   server.js

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
-var http = require('http');
-http.createServer(function (req, res){
-res.writeHead(200, {'Content-Type': 'text/plain'});
-res.end('Hello World\n');
-}).listen(8080, '127.0.0.1');
-console.log('Server running at http://127.0.0.1:8080/');
+const express = require('express')
+const app = express()
+
+app.get('/',function(req,res){
+
+     res.sendFile(__dirname + '/index.html');
+
+});
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
